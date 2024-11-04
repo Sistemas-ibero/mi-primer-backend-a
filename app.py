b7532398f1561ce897a952cd0959d446a54cd75e
mozta <perezaguirrerafael@gmail.com>
2024-11-04 16:43:16 -0600

    basic login

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
+
+usuario = {
+    'email': 'ejemplo@gmail.com',
+    'password': '123456'
+}
 
 @app.route('/')
 def home():
@@ -9,6 +14,17 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/success', methods=['POST'])
+def success():
+    email = request.form['email']
+    password = request.form['password']
+    print(email, password)
+    if email == usuario['email'] and password == usuario['password']:
+        return render_template('success.html')
+    else:
+        print("Llegale >:(")
+        return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
